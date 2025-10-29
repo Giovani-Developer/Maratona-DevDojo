eac74c1647265f2b3b9ebcd9ca30c96a2a67745c
Giovani Freitas <giovanifreitasdc@gmail.com>
2025-10-28 23:11:28 -0300

    Laços de Repetição

---
+++ src/Aula06EstruturasDeRepetição02.java
@@ -0,0 +1,9 @@
+public class Aula06EstruturasDeRepetição02 {
+    public static void main(String[] args) {
+        // Imprima todos os numeros pares de 0 até 1000000
+        for (int i = 0; i <= 1000000; i+= 2) {
+            if ( i % 2 == 0);
+            System.out.println(i);
+        }
+    }
+}
